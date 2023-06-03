09c942607aaefb6eb0061ce7d835c7e8c92f6150
bokkle <mitch.r_@hotmail.com>
2023-06-03 01:08:19 -0400

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./components/Nav";
+import React, { useState, useEffect } from "react";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
 
-function App() {
+const App = () => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (book) => {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  };
+
+  const changeQuantity = (book, quantity) => {
+    setCart(
+      cart.map((item) => {
+        if (item.id === book.id) {
+          return {
+            ...item,
+            quantity: +quantity,
+          };
+        } else {
+          return item;
+        }
+      })
+    );
+  };
+
+  const removeItem = (item) => {
+    setCart(cart.filter((book) => book.id !== item.id));
+    console.log("removeItem", item);
+  };
+
+  const numberOfItems = () => {
+    let counter = 0;
+    cart.forEach((item) => {
+      counter += item.quantity;
+    });
+    return counter;
+  };
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav numberOfItems={numberOfItems()} />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route
+          path="/books/:id"
+          render={() => (
+            <BookInfo books={books} addToCart={addToCart} cart={cart} />
+          )}
+        />
+        <Route
+          path="/cart"
+          render={() => (
+            <Cart
+              books={books}
+              cart={cart}
+              changeQuantity={changeQuantity}
+              removeItem={removeItem}
+            />
+          )}
+        />
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
